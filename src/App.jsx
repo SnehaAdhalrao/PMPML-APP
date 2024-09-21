6e640fac20302f8526c7d620e93f6d77bb370bab
SnehaAdhalrao <adhalraosneha000@gmail.com>
2024-09-22 00:14:18 +0530

    "Complaints page Added"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import PageTitle from './Components/PageTitle';
+import Complaits_page from './Components/Complaits_page';
 
 export default function App() {//   REMEMBER************just components bnane ke baad brach open krni hain
   return (
     <>
     <div className="">
       <PageTitle text={"Complaints"}/>
+      <Complaits_page/>
     </div>
     </>
   )
