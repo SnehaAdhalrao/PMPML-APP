8f6456cbb4e96c30744d3a378e259a7388314aae
SnehaAdhalrao <adhalraosneha000@gmail.com>
2024-09-22 00:14:18 +0530

    "Complaints page Added"

---
+++ src/Components/Complaits_page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Complaits_page() {
+  return (
+    <div className='bg-green-100'>
+      this is the complaints page
+    </div>
+  )
+}
+
+export default Complaits_page
