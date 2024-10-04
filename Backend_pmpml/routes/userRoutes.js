41ef686b2b6967264bdb442990e81f817fcb76a3
SnehaAdhalrao <adhalraosneha000@gmail.com>
2024-10-04 09:12:59 +0530

    Added PMPML-APP and Backend_pmpml to the repository

--- Backend_pmpml/routes/userRoutes.js
+++ Backend_pmpml/routes/userRoutes.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 // Register User
 router.post('/register', async (req, res) => {
   try {
     const { firstName, lastName, email, password, gender } = req.body;
-    const newUser = new User({ firstName, lastName, email, password, gender });
+    const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
+    const newUser = new User({ firstName, lastName, email, password: hashedPassword, gender });
     await newUser.save();
-    res.status(201).send('User registered successfully');
+    res.status(200).json({ success: true, message: 'User registered successfully' });
   } catch (error) {
-    res.status(400).send('Error registering user: ' + error.message);
+    res.status(400).json({ success: false, message: 'Error registering user: ' + error.message });
   }
 });
 
@@ -19,13 +21,13 @@ router.post('/register_or_login', async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (user && user.password === password) {
-      res.status(200).send('Login successful');
+    if (user && await bcrypt.compare(password, user.password)) { // Compare hashed passwords
+      res.status(200).json({ success: true, message: 'Login successful' });
     } else {
-      res.status(400).send('Invalid credentials');
+      res.status(200).json({ success: false, message: 'Invalid credentials' });
     }
   } catch (error) {
-    res.status(400).send('Error logging in: ' + error.message);
+    res.status(500).json({ success: false, message: 'Error logging in: ' + error.message });
   }
 });
 
