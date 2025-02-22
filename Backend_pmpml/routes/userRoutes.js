245735c45961797393b8739df242f075e507cc77
SnehaAdhalrao <adhalraosneha000@gmail.com>
2025-02-22 20:23:11 +0530

    Revert "Added PMPML-APP and Backend_pmpml to the repository"
    This reverts commit 0c3f2b5bb52f0c95bc73dfac493be050dba74d5a.

--- Backend_pmpml/routes/userRoutes.js
+++ Backend_pmpml/routes/userRoutes.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
 
 // Register User
 router.post('/register', async (req, res) => {
   try {
     const { firstName, lastName, email, password, gender } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
-    const newUser = new User({ firstName, lastName, email, password: hashedPassword, gender });
+    const newUser = new User({ firstName, lastName, email, password, gender });
     await newUser.save();
-    res.status(200).json({ success: true, message: 'User registered successfully' });
+    res.status(201).send('User registered successfully');
   } catch (error) {
-    res.status(400).json({ success: false, message: 'Error registering user: ' + error.message });
+    res.status(400).send('Error registering user: ' + error.message);
   }
 });
 
@@ -21,13 +19,13 @@ router.post('/register_or_login', async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (user && await bcrypt.compare(password, user.password)) { // Compare hashed passwords
-      res.status(200).json({ success: true, message: 'Login successful' });
+    if (user && user.password === password) {
+      res.status(200).send('Login successful');
     } else {
-      res.status(200).json({ success: false, message: 'Invalid credentials' });
+      res.status(400).send('Invalid credentials');
     }
   } catch (error) {
-    res.status(500).json({ success: false, message: 'Error logging in: ' + error.message });
+    res.status(400).send('Error logging in: ' + error.message);
   }
 });
 
